2fe6393608e82f68fd2ee37c2e97f6366f47ccc6
Arthur G <agraham@WINDELL-9DU9KRP.localdomain>
2017-11-27 14:33:37 +0000

    tricksy challenge

---
+++ src/tricksy-app/Aside.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// we use className to add classes for Bootstrap styling
+const Aside = () => (
+    <aside className="col-md-4">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Huius, Lyco, oratione locuples, rebus ipsis ielunior.</p>
+        <img className="img-thumbnail" src="http://78.media.tumblr.com/ea3884f17979500c2406fd4d21891195/tumblr_mr4humiiCH1r4xjo2o1_250.gif" />
+    </aside>
+);
+
+export default Aside;
---
+++ src/tricksy-app/Paragraph.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const Paragraph = () => (
+    <main className="col-md-8">
+    	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Huius, Lyco, oratione locuples, rebus ipsis ielunior</p>
+    </main>
+);
+
+export default Paragraph;
